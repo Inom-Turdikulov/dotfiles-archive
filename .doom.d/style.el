707559f0379ab0ba80dcc8cf6862b307d2beecca
Inom T <i@inomoz.ru>
2022-06-28 19:16:51 +0300

    Add transparency emacs support

--- .doom.d/style.el
+++ .doom.d/style.el
@@ -76,3 +76,12 @@
 ;; support cursor shape in terminal
 (use-package! evil-terminal-cursor-changer
   :hook (tty-setup . evil-terminal-cursor-changer-activate))
+
+;; Transparent background in tty
+(defun set-background-for-terminal (&optional frame)
+  (or frame (setq frame (selected-frame)))
+  "unsets the background color in terminal mode"
+  (unless (display-graphic-p frame)
+    (set-face-background 'default "unspecified-bg" frame)))
+(add-hook 'after-make-frame-functions 'set-background-for-terminal)
+(add-hook 'window-setup-hook 'set-background-for-terminal)
