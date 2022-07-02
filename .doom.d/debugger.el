4d9e028b7344feb499336fe112d4f06e6e3bcdbb
Inom T <i@inomoz.ru>
2022-07-02 14:21:59 +0300

    Adjust flask debug configuration

--- .doom.d/debugger.el
+++ .doom.d/debugger.el
@@ -4,7 +4,6 @@
 ;;
 
 (after! dap-mode
-  (require 'dap-python)
   (setq dap-python-debugger 'debugpy)
 
   (add-hook 'dap-stopped-hook
@@ -15,19 +14,47 @@
                                      :args "-i"
                                      :cwd nil
                                      :env '(
-                                            ("FLASK_APP" . "wsgi.py")
                                             ("OIDC_DEV" . "KEYCLOAK_AUTH_DISABLED")
+                                            ("FLASK_APP" . "wsgi.py")
                                             ("LOG_NAME" . "app")
                                             )
                                      :args (concat
                                             "run"
-                                            "--no-debugger"
-                                            "--no-reload"
+                                            "--no-debugger" ;; dap-debug had conflicts with built-in debugger
                                             )
                                      :request "launch"
-                                     :name "My App"))
-  )
+                                     :name "Flask")))
 
+(map! :map dap-mode-map
+      :leader
+      :prefix ("d" . "dap")
+      ;; basics
+      :desc "dap next"          "n" #'dap-next
+      :desc "dap step in"       "i" #'dap-step-in
+      :desc "dap step out"      "o" #'dap-step-out
+      :desc "dap continue"      "c" #'dap-continue
+      :desc "dap hydra"         "h" #'dap-hydra
+      :desc "dap debug restart" "r" #'dap-debug-restart
+      :desc "dap debug"         "s" #'dap-debug
+
+      ;; debug
+      :prefix ("dd" . "Debug")
+      :desc "dap debug recent"  "r" #'dap-debug-recent
+      :desc "dap debug last"    "l" #'dap-debug-last
+
+      ;; eval
+      :prefix ("de" . "Eval")
+      :desc "eval"                "e" #'dap-eval
+      :desc "eval region"         "r" #'dap-eval-region
+      :desc "eval thing at point" "s" #'dap-eval-thing-at-point
+      :desc "add expression"      "a" #'dap-ui-expressions-add
+      :desc "remove expression"   "d" #'dap-ui-expressions-remove
+
+      :prefix ("db" . "Breakpoint")
+      :desc "dap breakpoint toggle"      "b" #'dap-breakpoint-toggle
+      :desc "dap breakpoint condition"   "c" #'dap-breakpoint-condition
+      :desc "dap breakpoint hit count"   "h" #'dap-breakpoint-hit-condition
+      :desc "dap breakpoint log message" "l" #'dap-breakpoint-log-message)
 
 (use-package! ue
   :init   (ue-global-mode +1)
