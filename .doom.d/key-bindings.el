9ee15da2e28fd7566e29c0bdc08174a7f993890e
Inom T <i@inomoz.ru>
2022-07-28 12:27:09 +0300

    Add new keybindings

--- .doom.d/key-bindings.el
+++ .doom.d/key-bindings.el
@@ -1,6 +1,7 @@
 ;;; key-bindings.el -*- lexical-binding: t; -*-
 
 (global-set-key (kbd "C-'")   'avy-goto-char-2)
+(global-set-key (kbd "C-\"")   'avy-goto-line)
 (global-set-key (kbd "C-c z") 'zeal-at-point)
 (global-set-key (kbd "C-s")   'save-buffer)
 (global-set-key (kbd "C-c d") 'look-up-dict-marked)
@@ -9,9 +10,9 @@
 (global-set-key (kbd "C-<f1>") 'execute-c-program)
 (global-set-key (kbd "C-<f2>") 'speed-type-buffer)
 (global-set-key (kbd "C-<f3>") 'langtool-check)
+(global-set-key (kbd "C-c SPC") '+workspace/switch-to)
 (global-set-key (kbd "C-S-<f3>") 'langtool-check-done)
 (global-set-key (kbd "C-<f4>") (lambda() (interactive) (magit-status "/yadm::")))
-
 
 (use-package! elfeed
   :general
@@ -20,15 +21,22 @@
    ))
 
 ;; Leader key-maps
-(map! :leader
-      :desc "Anki editor add note"
-      "N a" #'Anne-editor-insert-note)
 
 (map! :leader
-      :desc "IRC client"
-      "o i" #'=irc)
+      :desc "emms"
+      "o e" (lambda() (interactive) (persp-switch "*EMMS*")))
+
+(map! :leader
+      :desc "Anki editor add note"
+      "N a" #'anki-editor-insert-note)
+
+(map! :leader
+      :desc "irc"
+      "o i t" (lambda() (interactive) (setq frame-title-format "%b - Bitlbee"))
+      "o i i" #'=irc)
 
 (map! :leader
       :desc "News (rss) client"
       "o n" #'=rss)
 
+;; CSV
